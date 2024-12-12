12911f8fd5a62c23a6245d5f646090e7c9bc4f2a
tranvanchung93123 <tranvanchung93123@gmail.com>
2024-12-12 06:50:19 -0800

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded');
+    const ellipseWrapper = document.querySelector('.ellipse-wrapper');
+    const notificationContainer = document.getElementById('notification-container');
+
+    ellipseWrapper.addEventListener('click', function() {
+        if (notificationContainer.style.display === 'none') {
+            notificationContainer.style.display = 'block';
+        } else {
+            notificationContainer.style.display = 'none';
+        }
+    });
+
+    const dashboardMenuItem = document.querySelector('#dashboard-nav');
+    const leaveMenuItem = document.querySelector('#leave-nav');
+    const dashboardSection = document.querySelector('.dashboard-section'); // Assuming this is the Dashboard section
+    const leaveRequestsSection = document.querySelector('.leave-section'); // Assuming this is the Leave Requests section
+
+    dashboardMenuItem.addEventListener('click', function() {
+        dashboardSection.style.display = 'block';
+        leaveRequestsSection.style.display = 'none';
+        dashboardMenuItem.classList.add('menu-item-wrapper');
+        leaveMenuItem.classList.remove('menu-item-wrapper');
+    });
+
+    leaveMenuItem.addEventListener('click', function() {
+        leaveRequestsSection.style.display = 'block';
+        dashboardSection.style.display = 'none';
+        dashboardMenuItem.classList.remove('menu-item-wrapper');
+        leaveMenuItem.classList.add('menu-item-wrapper');
+    });
+});
